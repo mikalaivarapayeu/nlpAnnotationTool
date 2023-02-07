86bc69daf9ec3714ffe02bf3fadc89137937612d
m_voropaev <voropaev.nv@gmail.com>
2023-02-07 15:21:47 +0300

    This my second update Started to work on the update sentence function which will send updates to mongoDB. Fixed problem with changing an active phrase label. Prepare sentence.ejs file for the implementation the update sentence function. Slightly changed css and bootstrap to improve styling.

--- public/javascripts/annotation.js
+++ public/javascripts/annotation.js
@@ -1,5 +1,7 @@
 const setLabels = document.querySelectorAll('#labels li');
 const words = document.querySelectorAll('.word');
+const updateSentArr = document.querySelectorAll('.updateSentence');
+const taggedWords = document.querySelectorAll('.taggedWord');
 
 function activeLabelToggle(e) {
     let activeLabelCurrent = document.querySelector('.activeLabel');
@@ -11,6 +13,7 @@ function untaggingWord(e) {
     const clickedElmUnTagged = this;
     const spanWordUntagged = document.createElement('span');
     spanWordUntagged.append(clickedElmUnTagged.firstElementChild.innerText);
+    spanWordUntagged.classList.add('word')
     spanWordUntagged.addEventListener('click', wordTagging)
     clickedElmUnTagged.replaceWith(spanWordUntagged)
 
@@ -33,8 +36,18 @@ function wordTagging(e) {
 
 }
 
+function updateSentence() {
+    const updateSent = {}
+    const semanticLabel = document.querySelector('#semanticLabel span')
+    const extractable = document.querySelector('#extractable span')
+    const isSelfContained = document.querySelector('#isSelfContained span')
+    const sentence = document.querySelector('.sentPlace')
+
+    console.log(semanticLabel.innerText)
+}
+
 setLabels.forEach(label => {
-    label.addEventListener('click', wordTagging)
+    label.addEventListener('click', activeLabelToggle)
 })
 
 
@@ -43,4 +56,12 @@ words.forEach(word => {
     word.addEventListener('click', wordTagging)
 })
 
-console.log(setLabels);
+
+taggedWords.forEach(taggedWord => {
+    taggedWord.addEventListener('click', untaggingWord)
+})
+
+
+updateSentArr.forEach(sent => {
+    sent.addEventListener('click', updateSentence)
+})

cbc52a4e87f8a305ddd38b9c005fa0d5b81bdaf1
m_voropaev <voropaev.nv@gmail.com>
2023-03-18 14:44:39 +0300

    Add feature to add a new word.

--- public/javascripts/sentenceManagement.js
+++ public/javascripts/sentenceManagement.js
@@ -2,7 +2,7 @@
 const newPairs = document.querySelectorAll('.btnNewPair');
 const modal = document.getElementById('Modal')
 const wordSave = document.querySelector("#saveWord")
-
+const addWords = document.querySelectorAll('.addWord');
 
 
 
@@ -16,31 +16,95 @@ modal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
     const buttonModal = event.relatedTarget;
     const closestWordLabelDiv = buttonModal.closest('.wordLabelPair');
-    const sentWord = closestWordLabelDiv.children[0].children[0].innerText;
-    const wordPlaceholder = document.querySelector('#sentWord');
-    wordPlaceholder.placeholder = sentWord;
-    wordPlaceholder.value = sentWord;
-    closestWordLabelDiv.classList.add('clickedPair');
-    // console.log(buttonModal)
+    if (buttonModal.innerText == 'Update Word') {
+        // const closestWordLabelDiv = buttonModal.closest('.wordLabelPair');
+        const sentWord = closestWordLabelDiv.children[0].children[0].innerText;
+        const wordPlaceholder = document.querySelector('#sentWord');
+        wordPlaceholder.placeholder = sentWord;
+        wordPlaceholder.value = sentWord;
+        closestWordLabelDiv.classList.add('updateWord');
+        // console.log(buttonModal)
+    } else {
+        const wordPlaceholder = document.querySelector('#sentWord');
+        wordPlaceholder.placeholder = '';
+        wordPlaceholder.value = '';
+        closestWordLabelDiv.classList.add('addWord');
+    }
+    // closestWordLabelDiv.classList.add('updateWord');
 
 })
-
-
 
 
 
 modal.addEventListener('hide.bs.modal', function (event) {
-    const clickedWordLabelPairDiv = document.querySelector('.clickedPair');
-    clickedWordLabelPairDiv.classList.remove('clickedPair')
+    const updateWordDiv = document.querySelector('.updateWord');
+    const addWordDiv = document.querySelector('.addWord');
+    if (updateWordDiv) {
+        updateWordDiv.classList.remove('updateWord')
+    }
+    if (addWordDiv) {
+        addWordDiv.classList.remove('addWord')
+    }
 })
 
 wordSave.addEventListener('click', function (event) {
-    const clickedWordDiv = document.querySelector('.clickedPair');
+    const updateWordDiv = document.querySelector('.updateWord');
+    const addWordDiv = document.querySelector('.addWord');
     const wordForm = document.querySelector('#sentWord');
-    const clickedWord = clickedWordDiv.children[0].children[0];
+    if (updateWordDiv) {
+        // const wordForm = document.querySelector('#sentWord');
+        const clickedWord = updateWordDiv.children[0].children[0];
+        clickedWord.innerText = wordForm.value
+    }
+    if (addWordDiv) {
+        if (wordForm.value) {
+            // Common elements
+            const rowDiv = document.createElement('div');
+            const spanWord = document.createElement('span');
+            // Words elements
+            rowDiv.classList.add('row', 'wordLabelPair');
+            const wordDiv = document.createElement('div');
+            wordDiv.classList.add('col-5', 'word')
+            const wordSpan = document.createElement('span');
+            // Label elelemnts
+            const labelDiv = document.createElement('div');
+            labelDiv.classList.add('col-3', 'label')
+            const labelSpan = document.createElement('span');
+            // Button elements
+            const buttonDiv = document.createElement('div');
+            buttonDiv.classList.add('col-4')
+            const updateButton = document.createElement('button')
+            updateButton.classList.add('btn', 'btn-primary', 'btn-sm')
+            const addButton = document.createElement('button')
+            addButton.classList.add('btn', 'btn-primary', 'btn-sm')
+            const deleteButton = document.createElement('button')
+            deleteButton.classList.add('btn', 'btn-primary', 'btn-sm')
+            // construct HTML frgament
+            wordSpan.innerText = wordForm.value
+            wordDiv.append(wordSpan)
+            labelSpan.innerText = 'none'
+            labelDiv.append(labelSpan)
+            updateButton.innerText = 'Update Word'
+            updateButton.setAttribute('data-bs-toggle', 'modal')
+            updateButton.setAttribute('data-bs-target', '#Modal')
+            buttonDiv.append(updateButton)
+            addButton.innerText = 'Add Word'
+            addButton.setAttribute('data-bs-toggle', 'modal')
+            addButton.setAttribute('data-bs-target', '#Modal')
+            buttonDiv.append(addButton)
+            deleteButton.innerText = 'Delete Word'
+            buttonDiv.append(deleteButton)
+            rowDiv.append(wordDiv)
+            rowDiv.append(labelDiv)
+            rowDiv.append(buttonDiv)
+            // Place element in correcrt place
+            console.log(addWordDiv)
+            addWordDiv.insertAdjacentElement('beforebegin', rowDiv)
+        }
+
+    }
     const selectedModal = bootstrap.Modal.getInstance(document.getElementById('Modal'));
-    console.log(clickedWordDiv)
-    clickedWord.innerText = wordForm.value
-    // clickedWordDiv.classList.remove('clickedPair')
     selectedModal.hide()
 })
+
+

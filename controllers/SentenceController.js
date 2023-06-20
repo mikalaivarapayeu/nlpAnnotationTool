1d44ed2b288876611269949f26a4aad4480f1dbd
m_voropaev <voropaev.nv@gmail.com>
2023-06-20 16:21:07 +0300

    Swithced all sentences controllers from mongoose do node mongoddb driver

--- controllers/SentenceController.js
+++ controllers/SentenceController.js
@@ -1,35 +1,26 @@
 // const Sentence = require('../models/sentence');
 // const Label = require('../models/nlpLabels');
 // const paginate = require('../utils/pagination')
-const { connectToDb, getDb } = require('./db');
+const { connect } = require('../db');
+const { ObjectId } = require('mongodb');
 
-let db;
 
-connectToDb((err) => {
-    if (!err) {
-        app.listen(3000, () => {
-            console.log('Serving on Port 3000')
-        })
-    }
 
-    db = getDb()
-})
+async function createObjectIdFilter(id) {
+    const objId = new ObjectId(id)
+    const filterObj = { _id: objId };
+    return filterObj
+}
+
 
 
 
 module.exports.index = async (req, res) => {
     try {
-        const name = req.query.name
-        // const page = parseInt(req.query.page);
-        // const sentences = res.paginatedResults;
-        // const totalNum = await Sentence.countDocuments().exec()
-        // console.log(totalNum)
-        // console.log('############')
-        // console.log(sentPag);
-        // const sentences = await Sentence.find({})
-        const labels = await Label.find({})
-        // console.log('############');
-        // console.log(req);
+        const page = parseInt(req.query.page);
+        const db = await connect();
+        const labels = await db.collection('labels').find().toArray();
+        const sentences = res.paginatedResults;
         res.render('sentences/sentence', { labels, sentences, page })
     } catch {
         console.log(error)
@@ -38,21 +29,19 @@ module.exports.index = async (req, res) => {
 };
 
 
+
+
+
 module.exports.duplicateSentence = async (req, res) => {
     try {
         const page = parseInt(req.query.page)
         const { id } = req.params;
-        const sent = await Sentence.findById(id)
-        const newSent = new Sentence({
-            sentNumber: sent.sentNumber,
-            semanticLabel: sent.semanticLabel,
-            isExtractable: sent.isExtractable,
-            isSelfContanined: sent.isSelfContanined,
-            sentWords: sent.sentWords
-        })
-        // console.log(`/sents?page=${page}`)
-        await newSent.save();
-        // res.redirect(`/sents?page=${page}`)
+        const db = await connect();
+        const filter = await createObjectIdFilter(id);
+        doc = await db.collection('sentences',).findOne(filter);
+        delete doc['_id']
+        // console.log(doc)
+        db.collection('sentences',).insertOne(doc)
         res.redirect(`/sents?page=${page}`)
     } catch {
         console.log(error)
@@ -62,39 +51,44 @@ module.exports.duplicateSentence = async (req, res) => {
 
 
 
+
 module.exports.updateSingleSentence = async (req, res) => {
     try {
         const { data } = req.body;
-        // console.log(data)
-        const sentence = await Sentence.findById(data.sentNumber)
-        const filter = { _id: data.sentNumber }
-        const update = {}
-        sentence.semanticLabel = data.semanticLabel
-        sentence.isExtractable = data.isExtractable
-        sentence.isSelfContanined = data.isSelfContanined
-        for (let updWord of data.updatedWordsAndIndx) {
-            let updSentIdx = parseInt(updWord[0]);
-            sentence.sentWords[updSentIdx][1] = updWord[1]
+        const page = parseInt(req.query.page)
+        const db = await connect();
+        const filter = await createObjectIdFilter(data.sentNumber);
+        const sentence = await db.collection('sentences',).findOne(filter);
+        const sentWordsUpdate = sentence.sentWords
+        for (let i = 0; i < data.updatedWordsAndIndx.length; i++) {
+            sentWordsUpdate[i][1] = data.updatedWordsAndIndx[i][1]
         }
-        // console.log(sentence)
-        let updSent = await Sentence.findOneAndUpdate(filter, sentence, {
-            new: true
-        });
-        req.flash('success', 'Successfully updated the sentence.')
-        // res.redirect('/sents?page=1')
+        const update = {
+            $set: {
+                semanticLabel: data.semanticLabel, isExtractable: data.isExtractable, isSelfContanined: data.isSelfContanined,
+                sentWords: sentWordsUpdate
+            }
+        }
+        console.log(update)
+        await db.collection('sentences').updateOne(filter, update);
+        // req.flash('success', 'Successfully updated the sentence.')
+        res.redirect(`/sents?page=${page}`)
     } catch {
         console.log(error)
     }
 
 };
 
+
+
+
 module.exports.deleteSingleSentence = async (req, res) => {
     try {
-        const page = parseInt(req.query.page)
+        const page = parseInt(req.query.page);
         const { id } = req.params;
-        const labels = await Sentence.findByIdAndDelete(id)
-        // console.log(page);
-
+        const db = await connect();
+        const filter = await createObjectIdFilter(id);
+        db.collection('sentences').deleteOne(filter)
         res.redirect(`/sents?page=${page}`)
     } catch {
         console.log(error)
@@ -104,10 +98,12 @@ module.exports.deleteSingleSentence = async (req, res) => {
 module.exports.sentenceDetails = async (req, res) => {
     try {
         const { id } = req.params;
-        const sentence = await Sentence.findById(id)
-        // console.log(sentence)
-        // console.log(constructedSentence)
-        res.render('sentences/sentenceDetails', { sentence })
+        const page = parseInt(req.query.page);
+        const db = await connect();
+        const filter = await createObjectIdFilter(id);
+        console.log(req.query)
+        const sentence = await db.collection('sentences',).findOne(filter);
+        res.render('sentences/sentenceDetails', { sentence, page })
     } catch {
         console.log(error)
     }
@@ -117,33 +113,53 @@ module.exports.sentenceDetails = async (req, res) => {
 module.exports.updateSentWords = async (req, res) => {
     try {
         const { data, sent_id } = req.body;
-        const sentence = await Sentence.findById(sent_id)
-        const filter = { _id: sent_id }
-        // console.log(sentence)
-        sentence.sentWords = data
-        // sentence.semanticLabel = sentence.semanticLabel
-        // sentence.isExtractable = sentence.isExtractable
-        // sentence.isSelfContanined = sentence.isExtractable
-        // for (let updWord of data.updatedWordsAndIndx) {
-        //     let updSentIdx = parseInt(updWord[0]);
-        //     // console.log(updWord)
-        //     // console.log(sentence.sentWords[updSentIdx])
-        //     sentence.sentWords[updSentIdx][1] = updWord[1]
-        // }
-        // console.log(data)
-        // console.log(sentence)
-        let updSent = await Sentence.findOneAndUpdate(filter, sentence, {
-            new: true
-        });
-        console.log('after update')
-        req.flash('success', 'Successfully updated the sentence.')
-        // res.redirect('sentences/sentenceDetails', { sentence })
+        const db = await connect();
+        const filter = await createObjectIdFilter(sent_id);
+        const update = {
+            $set: {
+                sentWords: data
+            }
+        }
+        await db.collection('sentences').updateOne(filter, update);
+        const sentence = await db.collection('sentences').findOne(filter);
+        console.log(sentence)
+        res.redirect('sentences/sentenceDetails', { sentence })
     } catch {
         console.log(error)
     }
 
 };
 
+
+// module.exports.updateSentWords = async (req, res) => {
+//     try {
+//         const { data, sent_id } = req.body;
+//         const sentence = await Sentence.findById(sent_id)
+//         const filter = { _id: sent_id }
+//         // console.log(sentence)
+//         sentence.sentWords = data
+//         // sentence.semanticLabel = sentence.semanticLabel
+//         // sentence.isExtractable = sentence.isExtractable
+//         // sentence.isSelfContanined = sentence.isExtractable
+//         // for (let updWord of data.updatedWordsAndIndx) {
+//         //     let updSentIdx = parseInt(updWord[0]);
+//         //     // console.log(updWord)
+//         //     // console.log(sentence.sentWords[updSentIdx])
+//         //     sentence.sentWords[updSentIdx][1] = updWord[1]
+//         // }
+//         // console.log(data)
+//         // console.log(sentence)
+//         let updSent = await Sentence.findOneAndUpdate(filter, sentence, {
+//             new: true
+//         });
+//         console.log('after update')
+//         req.flash('success', 'Successfully updated the sentence.')
+//         // res.redirect('sentences/sentenceDetails', { sentence })
+//     } catch {
+//         console.log(error)
+//     }
+
+// };
 
 
 
